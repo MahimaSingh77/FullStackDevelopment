4ef574ba7f8fdb2c3089964249554de764484a3e
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-12-26 12:42:49 +0530

    Comparision

--- 3_Javascript/01_Basics/04_comparision.js
+++ 3_Javascript/01_Basics/04_comparision.js
@@ -9,11 +9,18 @@
 // console.log("2" > 1);     //true
 // console.log("02" > 1);       //true
 
-console.log(null > 0);  //false
-console.log(null == 0);  //false
-console.log(null >= 0);  //true
 
-console.log(undefined == 0); //false
-console.log(undefined > 0); //false
-console.log(undefined < 0); //false
+/* Avoid these
+// console.log(null > 0);  //false    // null is converted to 0
+// console.log(null == 0);  //false     //it's an equity operator , it doesn't convert null to 0
+// console.log(null >= 0);  //true      // null is converted to 0
 
+// console.log(undefined == 0); //false
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+
+*/
+
+
+// ====
+console.log("2" === 2);   //false
