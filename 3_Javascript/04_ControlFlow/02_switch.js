1af7b0ae1ebda4a480f7e00fea7d5c5cc4f992dd
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-12-30 12:02:12 +0530

    switch

---
+++ 3_Javascript/04_ControlFlow/02_switch.js
@@ -0,0 +1,36 @@
+// const month = 3;
+
+// switch (month) {
+//   case 1:
+//     console.log("January");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 3:
+//     console.log("march");
+//     break;
+//   case 4:
+//     console.log("april");
+//     break;
+
+//   default:
+//     break;
+// }
+
+
+//string
+const month = "feb";
+
+switch (month) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("february");
+    break;
+
+
+  default:
+    break;
+}
