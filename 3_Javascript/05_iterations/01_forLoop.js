d6fe5129ba50b6f237aed2edc8dbe2879e5b3a4c
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-12-30 13:01:39 +0530

    forLoop

---
+++ 3_Javascript/05_iterations/01_forLoop.js
@@ -0,0 +1,19 @@
+//for loop
+
+for (let i = 0; i < 10; i++) {
+  const element = i;
+  console.log(element);
+}
+
+//nested loop
+
+// for (let i = 0; i < 5; i++) {
+//   const element = i;
+//   console.log(i);
+//   for (let j = 0; j < 5; j++) {
+//     const element2 = j;
+//     console.log(element2);
+//   }
+// }
+
+
