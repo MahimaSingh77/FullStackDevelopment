aa3f3b8380500c79e026bbe10fa4fa9c31596605
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-12-30 11:56:34 +0530

    Ifelse

---
+++ 3_Javascript/04_ControlFlow/01_ifelse.js
@@ -0,0 +1,45 @@
+// if
+
+// if(condition){
+// statements
+// }
+
+const isUserloggedIn = true
+if(isUserloggedIn){
+
+}
+
+2<=2
+// <, >, <=, >=, ++, !=, ===
+
+// ==  ->   (checks only values to be equal)
+// ===  ->   (checks  values as well as type to be equal)
+
+if(2!='3'){
+    console.log("executed");
+}
+
+
+
+//if else
+if(1>2){
+    console.log("1>2");
+}
+else{
+    console.log("1<2");
+}
+
+
+
+
+// const score = 200
+// if(score > 100){
+//    let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+
+ 
