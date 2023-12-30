2f0ae5deaba5bc0fcbfde5c77a7ed3153fe8da1f
MahimaSingh77 <mahima2454.be21@chitkara.edu.in>
2023-12-30 16:35:58 +0530

    Iterations

---
+++ 3_Javascript/05_iterations/06_others.js
@@ -0,0 +1,32 @@
+const coding = ["js", "ruby", "py"];
+
+// const values = coding.forEach((item)=>{
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);   //undefined
+
+const myNums = [1, 2, 3, 4, 5, 6, 10];
+
+// const newNums = myNums.filter((nums) => nums > 4);
+// const newNums = myNums.filter((nums) => {
+//   return nums > 4;
+// });
+// console.log(newNums);
+
+
+
+
+// const newNums = [];
+
+// myNums.forEach((num) => {
+//   if (num > 4) {
+//     newNums.push(num);
+//   }
+// });
+
+// console.log(newNums);
+
+
+
